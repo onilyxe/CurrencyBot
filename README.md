7d056f0236a0c5698ef16c4a9dca3ec5690d477e
Onilyxe <onilyxe@gmail.com>
2022-02-02 11:05:01 +0200

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+Add token and proxy list into settings.py
+To connect without proxy set empty proxy list
+Start: python __main__.py
--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@
 #socks5://148.251.234.93:1080/
 PROXIES_LIST=['']
 #xxxx:aaaaaa
-TOKEN='5122444757:AAG3c4sgNdGXnnpS_ozdJSaO1FM3MnOO8BY'
+TOKEN='telegrambotkey'
